a7e75350c4eb8e7280b4e24e58938d5e9c088cc8
nicolsss <56645701+nicolsss@users.noreply.github.com>
2022-01-12 12:09:33 -0500

    Inserts to sequences table

---
+++ scripts/sequences.sql
@@ -0,0 +1,3 @@
+insert into sequences (name, next_value) values ('passenger',1001);
+insert into sequences (name, next_value) values ('flight',1001);
+insert into sequences (name, next_value) values ('booking',1001);
